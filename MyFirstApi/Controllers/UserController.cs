f3d3fae9954b8396b5c91d0cc2e9b002ab6dbbff
Theo Rondon <theoprondon@hotmail.com>
2025-06-03 12:33:04 -0300

    atualizando projeto MyFirstApi com endpoints GET

--- MyFirstApi/Controllers/UserController.cs
+++ MyFirstApi/Controllers/UserController.cs
@@ -7,14 +7,31 @@ namespace MyFirstApi.Controllers;
 public class UserController : ControllerBase
 {
     [HttpGet]
+    [ProducesResponseType(typeof(User), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
     public IActionResult Get()
     {
-        var response = new Response
-        {
+        var response = new User
+        {            
             Age = 7,
             Name = "Theo"
         };
 
+        return Ok(response);
+    }    
+    
+    [HttpGet]
+    //[Route("{id}/{nickname}")] // passando valor pela rota
+    [ProducesResponseType(typeof(User), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
+    public IActionResult GetById([FromHeader] int id, [FromHeader] string? nickname) // passando valor pelo header
+    {
+        var response = new User
+        {
+            Id = 1,
+            Age = 7,
+            Name = "Theo"
+        };
 
         return Ok(response);
     }
--- MyFirstApi/obj/Debug/net8.0/MyFirstApi.AssemblyInfo.cs
+++ MyFirstApi/obj/Debug/net8.0/MyFirstApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyFirstApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a2553644aa807fb2dccc6d74f67008f7cb21b82")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyFirstApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyFirstApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
