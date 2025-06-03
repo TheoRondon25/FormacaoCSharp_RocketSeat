f074788b028319f4c3ba56f71d9179cdc3b602ff
Theo Rondon <theoprondon@hotmail.com>
2025-06-03 11:58:23 -0300

    Commite inicial, apenas criando o repositorio

---
+++ MyFirstApi/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyFirstApi.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class UserController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        var response = new Response
+        {
+            Age = 7,
+            Name = "Theo"
+        };
+
+
+        return Ok(response);
+    }
+}
