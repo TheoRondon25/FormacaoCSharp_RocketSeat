44d1672dabfa6eb4b7fbf72c890be87c4766b089
Theo Rondon <theoprondon@hotmail.com>
2026-03-31 20:34:42 -0300

    feat: Formatando numeros e colunas da planilha

--- CashFlow/src/CashFlow.Application/UseCases/Expenses/Reports/Excel/GenerateExpensesReportExcelUseCase.cs
+++ CashFlow/src/CashFlow.Application/UseCases/Expenses/Reports/Excel/GenerateExpensesReportExcelUseCase.cs
@@ -6,6 +6,7 @@ using CashFlow.Domain.Enums;
 namespace CashFlow.Application.UseCases.Expenses.Reports.Excel;
 public class GenerateExpensesReportExcelUseCase : IGenerateExpensesReportExcelUseCase
 {
+    private const string CURRENCY_SYMBOL = "R$";
     private readonly IExpensesReadOnlyRepository _repository;
 
     public GenerateExpensesReportExcelUseCase(IExpensesReadOnlyRepository repository)
@@ -24,8 +25,8 @@ public class GenerateExpensesReportExcelUseCase : IGenerateExpensesReportExcelUs
         var workbook = new XLWorkbook();
 
         workbook.Author = "Theo Rondon";
-        workbook.Style.Font.FontSize = 12;
-        workbook.Style.Font.FontName = "Times New Roman";       
+        workbook.Style.Font.FontSize = 10;
+        workbook.Style.Font.FontName = "Arial";       
 
         var worksheet = workbook.Worksheets.Add(month.ToString("Y"));
 
@@ -37,11 +38,16 @@ public class GenerateExpensesReportExcelUseCase : IGenerateExpensesReportExcelUs
             worksheet.Cell($"A{raw}").Value = expense.Title;
             worksheet.Cell($"B{raw}").Value = expense.Date;
             worksheet.Cell($"C{raw}").Value = ConvertPaymentType(expense.PaymentType);
+            
             worksheet.Cell($"D{raw}").Value = expense.Amount;
+            worksheet.Cell($"D{raw}").Style.NumberFormat.Format = $"-{CURRENCY_SYMBOL} #,##0.00";
+
             worksheet.Cell($"E{raw}").Value = expense.Description;
 
             raw++;
         }
+
+        worksheet.Columns().AdjustToContents();
 
         var file = new MemoryStream();
         workbook.SaveAs(file);
