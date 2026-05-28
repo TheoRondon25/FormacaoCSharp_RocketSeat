ac3f26ee4590a5002e781b39f9f43ec159f48627
Theo Rondon <theoprondon@hotmail.com>
2026-05-27 21:41:27 -0300

    feat: Criando a entitade Usuario(a) -> iniciando novo modulo de Autenticaçao, migration e testes de unidade.

---
+++ CashFlow/src/CashFlow.Domain/Enums/Roles.cs
@@ -0,0 +1,8 @@
+﻿using System.Globalization;
+
+namespace CashFlow.Domain.Enums;
+public static class Roles
+{
+    public const string ADMIN = "administrator";
+    public const string TEAM_MEMBER = "teamMember";
+}
